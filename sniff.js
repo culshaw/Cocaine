fc84478877f67abade372daa5a67cd3099c950d8
Ian Culshaw <culshaw@gmail.com>
2012-05-16 11:14:10 +0100

    Added detection for IE by way of maxConnectionsPerServer as a fallback for older IE browsers.
    Made the feature detect more robust by trawling through the window object in order to find properties prefixed with the vendors.

--- sniff.js
+++ sniff.js
@@ -1,12 +1,21 @@
-// Javascript sniffing technique by feature detection
+// Javascript sniffing technique by feature detection Ian Culshaw 2012!
 window.sniffer = function() {
 	var w = window
+	for(e in w) {
+		if(e.indexOf('ms') == 0) return 'ie'
+		if(e.indexOf('webkit') == 0) return 'webkit'
+		if(e.indexOf('moz') == 0) return 'firefox'			
+	}
+	
+	// some fallbacks
 	if(w.webkitRequestAnimationFrame) return 'chrome'
-	if(w.msRequestAnimationFrame) return 'ie'
+	if(w.msRequestAnimationFrame || w.maxConnectionsPerServer) return 'ie'
 	if(w.oRequestAnimationFrame) return 'opera'
 	if(w.mozRequestAnimationFrame) return 'firefox'
 	
 	// We still haven't picked the browser up, try a secondary measure
-	if(w.navigate) return 'opera' // Version 11.01
+	if(w.opera) return 'opera' // Version 11.01
 	if(w.chrome) return 'chrome'
 }
+
+console.log(sniffer())
